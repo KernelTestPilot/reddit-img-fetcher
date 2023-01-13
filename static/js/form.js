001432521773118472a206b0721e76eb9977afbe
KernelTestPilot <47748828+KernelTestPilot@users.noreply.github.com>
2023-01-13 19:09:22 +0100

    Add files via upload

--- static/js/form.js
+++ static/js/form.js
@@ -1,41 +1,28 @@
-$(document).ready(function() {
-
-	$('form').on('submit', function(event) {
-
+$(document).ready(function () {
+	$('form').on('submit', function (event) {
 		$.ajax({
-			data : {
-				name : $('#nameInput').val(),
-				type : $('#type').val(),
+			data: {
+				name: $('#nameInput').val(),
+				type: $('#type').val(),
 			},
-			type : 'POST',
-			url : '/process',
-			
+			type: 'POST',
+			url: '/process',
 		})
-		.done(function(data) {
-			
-			if (data.error) {
-				
-				$('#errorAlert').text(data.error).show();
-				console.log("rate limit")
-			}
-			else {
-				const m = data.title;
-			
-				$('.swiper-slide').remove();
-				$.each(data.name, function( index, value ) {
-					const title =m[index];
-					
-					$('#test').append("<div class='swiper-slide slide'><div class='content'> <img id='image' src="+value+"> <div class='content-overlay'><h2>"+title+"</h2> </div> </img> </div></div>");
-					
-				  });
-
-				$('#errorAlert').hide();
-			}
-
-		});
-
+			.done(function (data) {
+				if (data.error) {
+					$('#errorAlert').text(data.error).show();
+					console.log("rate limit")
+				}
+				else {
+					const m = data.title;
+					$('.swiper-slide').remove();
+					$.each(data.name, function (index, value) {
+						const title = m[index];
+						$('#test').append("<div class='swiper-slide slide'><div class='content'> <img id='image' src=" + value + "> <div class='content-overlay'><h2>" + title + "</h2> </div> </img> </div></div>");
+					});
+					$('#errorAlert').hide();
+				}
+			});
 		event.preventDefault();
-
 	});
-
 });
